3122779757c139d21bb7bc1f6a85708c88d0cb5e
Glaykiere <glaykiere@gmail.com>
2017-10-19 16:26:56 -0300

    Modelo usuario adicionado

---
+++ Projeto2/src/main/java/modelo/Usuario.java
@@ -0,0 +1,136 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ *
+ * @author glaykiere
+ */
+public class Usuario {
+    private String email;
+    private String nome;
+    private LocalDate nascimento;
+    private String sexo;
+    private String senha;
+    private final List<Movimentacao> movimentacao;
+
+    public Usuario(String email, String nome, LocalDate nascimento, String sexo, String senha) {
+        this.email = email;
+        this.nome = nome;
+        this.nascimento = nascimento;
+        this.sexo = sexo;
+        this.senha = senha;
+        this.movimentacao = new ArrayList<>();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(LocalDate nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 17 * hash + Objects.hashCode(this.email);
+        hash = 17 * hash + Objects.hashCode(this.nome);
+        hash = 17 * hash + Objects.hashCode(this.nascimento);
+        hash = 17 * hash + Objects.hashCode(this.sexo);
+        hash = 17 * hash + Objects.hashCode(this.senha);
+        hash = 17 * hash + Objects.hashCode(this.movimentacao);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Usuario other = (Usuario) obj;
+        if (!Objects.equals(this.email, other.email)) {
+            return false;
+        }
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        if (!Objects.equals(this.sexo, other.sexo)) {
+            return false;
+        }
+        if (!Objects.equals(this.senha, other.senha)) {
+            return false;
+        }
+        if (!Objects.equals(this.nascimento, other.nascimento)) {
+            return false;
+        }
+        if (!Objects.equals(this.movimentacao, other.movimentacao)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "email=" + email + ", nome=" + nome + ", nascimento=" + nascimento + ", sexo=" + sexo + ", senha=" + senha + ", movimentacao=" + movimentacao + '}';
+    }
+    
+    public boolean adicionarMovimentacao(Movimentacao m){
+        return movimentacao.add(m);
+    }
+    
+    public boolean autenticar(String email, String senha){
+        return this.email.equals(email) && this.senha.equals(senha);
+    }
+    
+    
+    
+    
+    
+}
